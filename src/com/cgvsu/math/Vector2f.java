97e1c67d6fb279dd0f2cdb4221e8b8043b4822a9
Sefger <sefger2006@gmail.com>
2025-10-27 14:41:57 +0300

    copy task3 from cgvsu

---
+++ src/com/cgvsu/math/Vector2f.java
@@ -0,0 +1,11 @@
+package com.cgvsu.math;
+
+// Это заготовка для собственной библиотеки для работы с линейной алгеброй
+public class Vector2f {
+    float x, y;
+
+    public Vector2f(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
