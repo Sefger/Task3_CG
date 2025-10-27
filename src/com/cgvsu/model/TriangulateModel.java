67fd0bbc392be5ec6966615127813480ed624eb6
Sefger <sefger2006@gmail.com>
2025-10-27 15:52:43 +0300

    create Triangulator

--- src/com/cgvsu/model/TriangulateModel.java
+++ src/com/cgvsu/model/TriangulateModel.java
@@ -2,13 +2,14 @@ package com.cgvsu.model;
 
 import java.util.ArrayList;
 
-public class TriangulateModel extends  Model{
-    public TriangulateModel(){
+public class TriangulateModel extends  Model {
+    public TriangulateModel() {
         //СУПЕР!!!
         super();
     }
+
     //на всякий случай вдруг кто захочет
-    public TriangulateModel(Model model){
+    public TriangulateModel(Model model) {
         this.vertices = new ArrayList<>(model.vertices);
         this.textureVertices = new ArrayList<>(model.textureVertices);
         this.normals = new ArrayList<>(model.normals);
@@ -19,19 +20,20 @@ public class TriangulateModel extends  Model{
     /*
     Хотя наверно это плохая идея, ведь класс уже сразу должен протриангулировать, надо будет уточнить!!!
      */
-    public int getTriangleCount(){
+    public int getTriangleCount() {
         int count = 0;
-        for (Polygon polygon: polygons){
-            if (polygon.getVertexIndices().size()==3){
+        for (Polygon polygon : polygons) {
+            if (polygon.getVertexIndices().size() == 3) {
                 count++;
             }
         }
         return count;
     }
-    public boolean isFullyTriangulated(){
+
+    public boolean isFullyTriangulated() {
         //можно было бы использовать getTriangleCount, но это долго
-        for (Polygon polygon: polygons){
-            if (polygon.getVertexIndices().size()!=3){
+        for (Polygon polygon : polygons) {
+            if (polygon.getVertexIndices().size() != 3) {
                 return false;
             }
         }
@@ -40,22 +42,22 @@ public class TriangulateModel extends  Model{
 
     //Провервка валидности триангулированной модели
 
-    public boolean isValid(){
-        for (Polygon polygon: polygons){
-            if (polygon.getVertexIndices().size()!=3) return false;
+    public boolean isValid() {
+        for (Polygon polygon : polygons) {
+            if (polygon.getVertexIndices().size() != 3) return false;
 
-            for (int vertexIndex: polygon.getVertexIndices()){
-                if (vertexIndex<0 || vertexIndex>=vertices.size()) return false;
+            for (int vertexIndex : polygon.getVertexIndices()) {
+                if (vertexIndex < 0 || vertexIndex >= vertices.size()) return false;
             }
 
-            if (!polygon.getVertexIndices().isEmpty()){
-                for (int texIndex: polygon.getTextureVertexIndices()){
-                    if (texIndex<0 ||texIndex>= textureVertices.size()) return false;
+            if (!polygon.getVertexIndices().isEmpty()) {
+                for (int texIndex : polygon.getTextureVertexIndices()) {
+                    if (texIndex < 0 || texIndex >= textureVertices.size()) return false;
                 }
             }
-            if (!polygon.getNormalIndices().isEmpty()){
-                for (int normalIndex: polygon.getNormalIndices()){
-                    if (normalIndex<0 || normalIndex >=normals.size()) return false;
+            if (!polygon.getNormalIndices().isEmpty()) {
+                for (int normalIndex : polygon.getNormalIndices()) {
+                    if (normalIndex < 0 || normalIndex >= normals.size()) return false;
                 }
             }
 
---
+++ src/com/cgvsu/model/Triangulator.java
@@ -0,0 +1,100 @@
+package com.cgvsu.model;
+
+import java.util.ArrayList;
+
+// Для выполнения триангуляции модели
+public class Triangulator {
+    //Нужен алгоритм веера
+    public TriangulateModel triangulateModel(Model model) {
+        TriangulateModel triangulateModel = new TriangulateModel(model);
+        //очищаем для новой триангуляции!!!
+        triangulateModel.polygons.clear();
+        for (Polygon orPolygon : model.polygons) {
+            ArrayList<Polygon> triangles = triangulatePolygon(orPolygon);
+            triangulateModel.polygons.addAll(triangles);
+        }
+        return triangulateModel;
+    }
+
+
+    //Не лучшая идея, кажется об этом говорили на лекции...
+    //Слишком много if хотелось бы уменьшить, но как не знаю
+    private ArrayList<Polygon> triangulatePolygon(Polygon polygon) {
+        ArrayList<Polygon> triangles = new ArrayList<>();
+        // в теории можно было бы и обойтись
+        ArrayList<Integer> vertexIndices = polygon.getVertexIndices();
+        ArrayList<Integer> textureIndices = polygon.getTextureVertexIndices();
+        ArrayList<Integer> normalIndices = polygon.getNormalIndices();
+        int vertexCount = vertexIndices.size();
+        if (vertexCount < 3) {
+            //not valid
+            return triangles;
+        }
+        //если уже треугольник, то ничего не менеям
+        if (vertexCount == 3) {
+            triangles.add(polygon);
+            return triangles;
+        }
+
+
+        boolean hasTexture = !textureIndices.isEmpty() && textureIndices.size() == vertexCount;
+        boolean hasNormals = !normalIndices.isEmpty() && normalIndices.size() == vertexCount;
+
+        for (int i = 1; i < vertexCount - 1; i++) {
+            Polygon triangle = new Polygon();
+            ArrayList<Integer> triangleVertexIndices = new ArrayList<>();
+            ArrayList<Integer> triangleTextureIndices = new ArrayList<>();
+            ArrayList<Integer> triangleNormalIndices = new ArrayList<>();
+
+            //первая вершина всегда вершина 0
+            triangleVertexIndices.add(vertexIndices.getFirst());
+            if (hasTexture) triangleTextureIndices.add(textureIndices.getFirst());
+            if (hasNormals) triangleNormalIndices.add(normalIndices.getFirst());
+
+            //вторая
+            triangleVertexIndices.add(vertexIndices.get(i));
+            if (hasTexture) triangleTextureIndices.add(textureIndices.get(i));
+            if (hasNormals) triangleNormalIndices.add(normalIndices.get(i));
+
+            //третья вершина
+            triangleVertexIndices.add(vertexIndices.get(i + 1));
+            if (hasTexture) triangleTextureIndices.add(textureIndices.get(i + 1));
+            if (hasNormals) triangleNormalIndices.add(normalIndices.get(i + 1));
+
+            triangle.setVertexIndices(triangleVertexIndices);
+            if (hasTexture) triangle.setTextureVertexIndices(triangleTextureIndices);
+            if (hasNormals) triangle.setNormalIndices(triangleNormalIndices);
+
+            triangles.add(triangle);
+
+        }
+        return triangles;
+    }
+
+    public static boolean needsTriangulation(Model model) {
+        for (Polygon polygon : model.polygons) {
+            if (polygon.getVertexIndices().size() > 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //Статистика
+
+    public static String getPolygonStatistics(Model model) {
+        int triangleCount = 0;
+        int quadCount = 0;
+        int ngonCount = 0;
+        for (Polygon polygon: model.polygons){
+            int vertexCount= polygon.getVertexIndices().size();
+            if (vertexCount==3){
+                triangleCount++;
+            }
+            else if (vertexCount==4){quadCount++;}
+            else  ngonCount++;
+        }
+        return String.format("Triangles: %d, Quads: %d, N-gons: %d", triangleCount, quadCount, ngonCount);
+    }
+}
